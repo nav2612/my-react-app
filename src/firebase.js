43645442a01a462428d19d242a1b5cb04195e3bd
nav2612 <navpreet51198@gmail.com>
2023-01-30 14:57:47 -0800

    30-01-2023 #2

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDW_3ITn9ow55SkI7lj7cNJVRBIMih5zLA",
+  authDomain: "open-house-deals.firebaseapp.com",
+  projectId: "open-house-deals",
+  storageBucket: "open-house-deals.appspot.com",
+  messagingSenderId: "855971901204",
+  appId: "1:855971901204:web:392129bc850adf5ddddc01",
+  measurementId: "G-HMZP5P01G5"
+};
+
+firebase.initializeApp(firebaseConfig);
+var auth = firebase.auth();
+export {auth , firebase};
--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-function Car(props) {
-  return <li>I am a { props.brand }</li>;
-}
-
-function Garage() {
-  const cars = ['Ford', 'BMW', 'Audi'];
-  return (
-    <>
-      <h1>Who lives in my garage?</h1>
-      <ul>
-        {cars.map((car) => <Car brand={car} />)}
-      </ul>
-    </>
-  );
-}
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Garage />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
