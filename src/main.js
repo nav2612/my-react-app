a622fcf1075d8871f22d31b896589df455360651
nav2612 <navpreet51198@gmail.com>
2023-02-01 11:08:28 -0800

    01-02-2023 #1

--- src/main.js
+++ src/main.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { auth } from './firebase';
+import './main.css';
+import Cards from './cards.js';
 
 const Mainpage = () => {
-
 	const logout = () => {
 		auth.signOut();
 	}
 
 	return (
-		<div style={{ marginTop: 250 }}>
+    <div>
+		<Cards/>
+		 <div className= "main-page" style={{ marginTop: 250 }}>
 			<center>
-				<h3>Welcome{auth.currentUser.phoneNumber}</h3>
-				<button style={{ "marginLeft": "20px" }}
-				onClick={logout}>Logout</button>
-				<button style={{ "marginLeft": "60px" }}>Realtor </button>
+			<h3>Welcome{auth.currentUser.phoneNumber}</h3>
+	 		<button style={{ "marginLeft": "20px" }}
+				onClick={logout}>Logout</button><br/>
+				<button style={{ "marginLeft": "20px" }}>Realtor </button>
 			</center>
 		</div>
+    </div>
 	);
 }
 
