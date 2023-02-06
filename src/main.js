11fae7af7f7891b2a8f01d7d11fe4ddac303d005
nav2612 <navpreet51198@gmail.com>
2023-02-06 15:14:02 -0800

    check

--- src/main.js
+++ src/main.js
@@ -7,12 +7,11 @@ import Cards from './Cards.js';
 const Mainpage = () => {
 
 	return (
-    <React.Fragment> 
+    <> 
     <Search/>
-    {console.log("hello didi")}
-    <Cards/>
+
     <Location/>
-    </React.Fragment>
+    </>
 	);
 }
 
